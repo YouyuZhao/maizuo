9ef1cefc180caa989ffcda6737a13efe5e446ad8
YouyuZhao <1213366162@qq.com>
2020-04-28 16:04:58 +0800

    1px像素mixins问题

--- src/views/Home/Films/index.vue
+++ src/views/Home/Films/index.vue
@@ -33,13 +33,15 @@ export default {
 </script>
 
 <style lang='scss'>
+// 引入公共的mixin
+@import '../../../assets/styles/mixins.scss';
+
 .tabs-bar-wrapper {
   position: relative;
   z-index: 100;
   width: 100%;
   overflow-x: hidden;
   background: #fff;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   a {
     text-decoration: none;
   }
@@ -50,80 +52,50 @@ export default {
     list-style: none;
   }
   .tabs-bar {
+    @include border-bottom;
     height: 49px;
-    display: -webkit-box;
-    display: -ms-flexbox;
     display: flex;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
     align-items: center;
     overflow-x: auto;
     overflow-y: hidden;
     transition: transform 0.2s cubic-bezier(0.35, 0, 0.25, 1);
-    transition: transform 0.2s cubic-bezier(0.35, 0, 0.25, 1),
-      -webkit-transform 0.2s cubic-bezier(0.35, 0, 0.25, 1);
     position: relative;
     .tabs-nav {
-      display: -webkit-box;
-      display: -ms-flexbox;
       display: flex;
-      display: -webkit-flex;
-      -webkit-box-pack: start;
-      -ms-flex-pack: start;
-      justify-content: flex-start;
-      -webkit-box-align: center;
-      -ms-flex-align: center;
+      justify-content: center;
       align-items: center;
       position: relative;
       width: 100%;
+      li {
+        flex-shrink: 0;
+        color: #191a1b;
+        text-align: center;
+        height: 16px;
+        line-height: 16px;
+        font-size: 14px;
+        cursor: pointer;
+      }
     }
-  }
-  .tabs-bar:after {
-    content: ' ';
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    height: 1px;
-    border-bottom: 1px solid #ededed;
-    color: #ededed;
-    -webkit-transform-origin: 0 100%;
-    -ms-transform-origin: 0 100%;
-    transform-origin: 0 100%;
-    -webkit-transform: scaleY(0.5);
-    -ms-transform: scaleY(0.5);
-    transform: scaleY(0.5);
   }
 }
 
-.tabs-bar-wrapper .tabs-bar .tabs-nav li {
-  -ms-flex-negative: 0;
-  flex-shrink: 0;
-  -webkit-flex-shrink: 0;
-  color: #191a1b;
-  text-align: center;
-  height: 16px;
-  line-height: 16px;
-  font-size: 14px;
-  cursor: pointer;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-}
 // .tabs-bar-wrapper .tabs-bar .tabs-nav li.active {
 //   color: #ff5f16;
 // }
-.tabs-bar-wrapper .tabs-bar .tabs-nav .tab-ink-bar-wrapper {
-  position: absolute;
-  margin: auto;
-  top: 30px;
-  left: 0;
-  -webkit-transform: translateZ(0);
-  transform: translateZ(0);
-  transition: transform 0.2s cubic-bezier(0.35, 0, 0.25, 1);
-}
-// .tabs-bar-wrapper .tabs-bar .tabs-nav .tab-ink-bar-wrapper .tab-ink-bar {
-//   border-bottom: 2px solid #ff5f16;
-//   border-radius: 20px;
-//   display: block;
+
+// .tabs-bar-wrapper .tabs-bar .tabs-nav .tab-ink-bar-wrapper {
+//   position: absolute;
 //   margin: auto;
+//   top: 30px;
+//   left: 0;
+//   -webkit-transform: translateZ(0);
+//   transform: translateZ(0);
+//   transition: transform 0.2s cubic-bezier(0.35, 0, 0.25, 1);
+//   .tab-ink-bar {
+//     border-bottom: 2px solid #ff5f16;
+//     border-radius: 20px;
+//     display: block;
+//     margin: auto;
+//   }
 // }
 </style>
