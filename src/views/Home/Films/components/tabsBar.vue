83cffe90888a218cd1490394f9d2b824b74d410f
YouyuZhao <1213366162@qq.com>
2020-05-03 22:43:33 +0800

    tabsBar组件抽离

---
+++ src/views/Home/Films/components/tabsBar.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="tabs-bar-wrapper">
+    <div class="tabs-bar">
+      <ul class="tabs-nav">
+        <li
+          class=""
+          style="width: 50%;"
+        >
+          <router-link to='home/films/now' />
+          <span>正在热映</span></li>
+        <li
+          class="active"
+          style="width: 50%;"
+        >
+          <router-link to='home/films/comming' />
+          <span>即将上映</span></li>
+        <div
+          class="tab-ink-bar-wrapper"
+          style="transform: translate3d(100%, 0px, 0px); width: 50%;"
+        ><span
+            class="tab-ink-bar"
+            style="width: 56px;"
+          ></span></div>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tabsBar'
+}
+</script>
+
+<style lang='scss' scoped>
+// 引入公共的mixin
+@import '@/assets/styles/mixins.scss';
+
+.tabs-bar-wrapper {
+  // position: relative;
+  z-index: 100;
+  width: 100%;
+  overflow-x: hidden;
+  background: #fff;
+  a {
+    text-decoration: none;
+  }
+  ul,
+  li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  .tabs-bar {
+    height: 49px;
+    display: flex;
+    align-items: center;
+    overflow-x: auto;
+    overflow-y: hidden;
+    transition: transform 0.2s cubic-bezier(0.35, 0, 0.25, 1);
+    position: relative;
+    @include border-bottom;
+    .tabs-nav {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      width: 100%;
+      li {
+        flex-shrink: 0;
+        color: #191a1b;
+        text-align: center;
+        height: 16px;
+        line-height: 16px;
+        font-size: 14px;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home/Films/index.vue
+++ src/views/Home/Films/index.vue
@@ -13,33 +13,7 @@
       </Swiper>
       <!-- 轮播图end -->
 
-      <!-- tabs-bar-begin -->
-      <div class="tabs-bar-wrapper">
-        <div class="tabs-bar">
-          <ul class="tabs-nav">
-            <li
-              class=""
-              style="width: 50%;"
-            >
-              <router-link to='home/films/now' />
-              <span>正在热映</span></li>
-            <li
-              class="active"
-              style="width: 50%;"
-            >
-              <router-link to='home/films/comming' />
-              <span>即将上映</span></li>
-            <div
-              class="tab-ink-bar-wrapper"
-              style="transform: translate3d(100%, 0px, 0px); width: 50%;"
-            ><span
-                class="tab-ink-bar"
-                style="width: 56px;"
-              ></span></div>
-          </ul>
-        </div>
-      </div>
-      <!-- tabs-bar-end -->
+      <tabsBar></tabsBar>
     </div>
 
     <router-view />
@@ -49,13 +23,15 @@
 <script>
 // 引入swiper核心与样式
 import { Swiper, SwiperItem } from '@/components/Swiper'
+import tabsBar from './components/tabsBar'
 import { getBanner } from '@/api/films'
 
 export default {
   name: 'Films',
   components: {
     Swiper,
-    SwiperItem
+    SwiperItem,
+    tabsBar
   },
   data () {
     return {
@@ -80,56 +56,11 @@ export default {
 </script>
 
 <style lang='scss'>
-// 引入公共的mixin
-@import '../../../assets/styles/mixins.scss';
-
 .films {
   height: 100%;
   display: flex;
   flex-direction: column;
 
-  .tabs-bar-wrapper {
-    // position: relative;
-    z-index: 100;
-    width: 100%;
-    overflow-x: hidden;
-    background: #fff;
-    a {
-      text-decoration: none;
-    }
-    ul,
-    li {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-    }
-    .tabs-bar {
-      height: 49px;
-      display: flex;
-      align-items: center;
-      overflow-x: auto;
-      overflow-y: hidden;
-      transition: transform 0.2s cubic-bezier(0.35, 0, 0.25, 1);
-      position: relative;
-      @include border-bottom;
-      .tabs-nav {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        width: 100%;
-        li {
-          flex-shrink: 0;
-          color: #191a1b;
-          text-align: center;
-          height: 16px;
-          line-height: 16px;
-          font-size: 14px;
-          cursor: pointer;
-        }
-      }
-    }
-  }
   .now {
     flex: 1;
     overflow-y: auto;
